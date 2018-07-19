00037d5f58893efbb71313cb0618ce7d4270da69
Nathan Gathright <nathan.gathright@gmail.com>
2018-07-19 15:05:48 -0500

    Update plasso.init.js

--- static/js/plasso.init.js
+++ static/js/plasso.init.js
@@ -3,7 +3,7 @@ Plasso.Cart.setup({
   spaceId: "TbHgLTRPAY",
   metadata: {
     plans: {
-      "my_plan_id": { "custom_url" },
+      "my_plan_id": "custom_url",
       "custom_path" : { "" },
       "itunes_id" : { "" },
       "spotify_link" : { "" }
