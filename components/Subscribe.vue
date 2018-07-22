267508a02b7fc1e373f271b5cd763ddd45c76ccd
Nathan Gathright <ngathright@insite.net>
2018-07-22 15:21:00 -0500

    Remove visibility Links

--- components/Subscribe.vue
+++ components/Subscribe.vue
@@ -2,15 +2,15 @@
   <div class="show-subscribe">
     <h2 class="show-subhead">Subscribe</h2>
     <div class="show-buttons">
-      <a class="show-link" target="_blank" rel="noopener" v-show="$store.state.itunesVisible" :href="`https://geo.itunes.apple.com/ca/podcast/feed/id${iTunesID}`"><img class="app-img" alt="iTunes" src="/images/podcatchers/icon-itunes.svg" /></a>
-      <a class="show-link" target="_blank" rel="noopener" v-show="$store.state.googleVisible" :href="googleLink"><img class="app-img" alt="Google Podcasts" src="/images/podcatchers/icon-googlepodcasts.svg" /></a>
-      <a class="show-link" target="_blank" rel="noopener" v-show="$store.state.overcastVisible" :href="`https://overcast.fm/itunes${iTunesID}`"><img class="app-img" alt="Overcast" src="/images/podcatchers/icon-overcast.svg" /></a>
-      <a class="show-link" target="_blank" rel="noopener" v-show="$store.state.castboxVisible" :href="`http://castbox.fm/vic/${iTunesID}`"><img class="app-img" alt="Castbox" src="/images/podcatchers/icon-castbox.svg" /></a>
-      <a class="show-link" target="_blank" rel="noopener" v-show="$store.state.pocketcastsVisible" :href="`http://pca.st/itunes/${iTunesID}`"><img class="app-img" alt="Pocket Casts" src="/images/podcatchers/icon-pocketcasts.svg" /></a>
-      <a class="show-link" target="_blank" rel="noopener" v-show="$store.state.castroVisible" :href="`https://castro.fm/itunes/${iTunesID}`"><img class="app-img" alt="Castro" src="/images/podcatchers/icon-castro.svg" /></a>
-      <a class="show-link" target="_blank" rel="noopener" v-show="$store.state.radiopublicVisible" :href="radiopublicLink"><img class="app-img" alt="RadioPublic" src="/images/podcatchers/icon-radiopublic.svg" /></a>
-      <a class="show-link" target="_blank" rel="noopener" v-show="$store.state.breakerVisible" :href="breakerLink"><img class="app-img" alt="Breaker" src="/images/podcatchers/icon-breaker.svg" /></a>
-      <a class="show-link" target="_blank" rel="noopener" v-show="$store.state.rssVisible" :href="`${this.feedUrl}`"><img class="app-img" alt="RSS" src="/images/podcatchers/icon-rss.svg" /></a>
+      <a class="show-link" target="_blank" rel="noopener" :href="`https://geo.itunes.apple.com/ca/podcast/feed/id${iTunesID}`"><img class="app-img" alt="iTunes" src="/images/podcatchers/icon-itunes.svg" /></a>
+      <a class="show-link" target="_blank" rel="noopener" :href="googleLink"><img class="app-img" alt="Google Podcasts" src="/images/podcatchers/icon-googlepodcasts.svg" /></a>
+      <a class="show-link" target="_blank" rel="noopener" :href="`https://overcast.fm/itunes${iTunesID}`"><img class="app-img" alt="Overcast" src="/images/podcatchers/icon-overcast.svg" /></a>
+      <a class="show-link" target="_blank" rel="noopener" :href="`http://castbox.fm/vic/${iTunesID}`"><img class="app-img" alt="Castbox" src="/images/podcatchers/icon-castbox.svg" /></a>
+      <a class="show-link" target="_blank" rel="noopener" :href="`http://pca.st/itunes/${iTunesID}`"><img class="app-img" alt="Pocket Casts" src="/images/podcatchers/icon-pocketcasts.svg" /></a>
+      <a class="show-link" target="_blank" rel="noopener" :href="`https://castro.fm/itunes/${iTunesID}`"><img class="app-img" alt="Castro" src="/images/podcatchers/icon-castro.svg" /></a>
+      <a class="show-link" target="_blank" rel="noopener" :href="radiopublicLink"><img class="app-img" alt="RadioPublic" src="/images/podcatchers/icon-radiopublic.svg" /></a>
+      <a class="show-link" target="_blank" rel="noopener" :href="breakerLink"><img class="app-img" alt="Breaker" src="/images/podcatchers/icon-breaker.svg" /></a>
+      <a class="show-link" target="_blank" rel="noopener" :href="`${this.feedUrl}`"><img class="app-img" alt="RSS" src="/images/podcatchers/icon-rss.svg" /></a>
     </div>
   </div>
 </template>
