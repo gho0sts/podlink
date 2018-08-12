1e1c467adaef2bf14014bed1823a3f3fcc36e5fc
Nathan Gathright <nathan.gathright@gmail.com>
2018-08-12 16:22:47 -0500

    Update Media Player

--- components/Listen.vue
+++ components/Listen.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="show-listen">
-    <h2 class="show-listen-subhead">Listen</h2>
     <no-ssr>
+      <div class="show-player-wrapper">
+        <div class="show-player-title">{{show.episodes[0].title}}</div>
       <vue-plyr v-bind:options="options">
         <audio id="show-player" style="width: 100%;" controls preload="none">
             <source :src="show.episodes[0].enclosure.url" type="audio/mp3">
         </audio>
       </vue-plyr>
+      </div>
     </no-ssr>
   </div>
 </template>
@@ -31,39 +33,90 @@ export default {
 .show-listen {
   width: 100%;
 
+  .show-player-wrapper {
+    position: relative;
+  }
+
+  .show-player-title {
+    position: absolute;
+    left: 5rem;
+    top: 1rem;
+    right: 1rem;
+    font-weight: bold;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    text-align: left;
+
+    @media screen and (min-width:44rem) {
+      font-size: 1.5rem;
+      left: 6rem;
+    }
+  }
+
   .plyr {
     z-index: 1;
   }
 
   .plyr--audio .plyr__controls {
-    margin-top: .25rem;
-    box-shadow: 0 0 2em -.5rem var(--brandMuted);
-    border-radius: .5rem;
-    padding: 0 .125rem;
     background: var(--brandBgTint);
+    border-radius: var(--borderradius);
+    padding: 1rem 1rem 0.625rem 1rem;
+    align-items: flex-end;
+  }
+
+  .plyr--audio .plyr__controls > .plyr__control:first-child {
+    background: var(--brandMuted);
+    border-radius: 50%;
+    width: 3rem;
+    height: 3rem;
+    padding: 0 0 0 15px;
+    margin-bottom: 6px;
+
+    &:hover {
+      transform: scale(1.166666667);
+    }
 
     @media screen and (min-width:44rem) {
-      margin-top: 0;
-      box-shadow: none;
+      width: 4rem;
+      height: 4rem;
+      padding: 0 0 0 20px;
     }
   }
 
-  .plyr--audio .plyr__control.plyr__tab-focus, 
-  .plyr--audio .plyr__control:hover, 
-  .plyr--audio .plyr__control[aria-expanded=true] {
-    background: var(--brandPrimary);
+  .plyr--audio .plyr__controls > .plyr__control:first-child svg {
+    width: 1.25rem;
+    height: 3rem;
+    fill: #FFF;
+
+    @media screen and (min-width:44rem) {
+      width: 1.75rem;
+      height: 4rem;
+    }
+  }
+
+  .plyr__controls .plyr__progress {
+    padding: 6px 0;
+    margin-left: 1rem;
+  }
+
+  .plyr__controls .plyr__time {
+    padding: 9px 0px;
+    line-height: 1;
+  }
+
+  .plyr--audio .plyr__control.plyr__tab-focus, .plyr--audio .plyr__control:hover, .plyr--audio .plyr__control[aria-expanded=true] {
+    background: transparent;
+    color: #4f5b5f;
+    transition: all .2s ease;
+  }
+
+  .plyr--audio .plyr__control.plyr__tab-focus svg, .plyr--audio .plyr__control:hover svg, .plyr--audio .plyr__control[aria-expanded=true] svg {
+    fill: var(--brandPrimary);
   }
 
   .plyr--full-ui input[type=range] {
     color: var(--brandPrimary);
-  }
-}
-
-.show-listen-subhead {
-  margin: 1.5rem 0 0;
-
-  @media screen and (min-width:44rem) {
-    display: none;
   }
 }
 
