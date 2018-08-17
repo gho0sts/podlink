25a3b43b6b0bdddd85f89a6aee941bf562bbe5a6
Nathan Gathright <nathan.gathright@gmail.com>
2018-08-17 15:37:26 -0500

    Update Store

--- store/index.js
+++ store/index.js
@@ -3,16 +3,12 @@ import Vuex from 'vuex'
 const createStore = () => {
     return new Vuex.Store({
         state: {
-            itunesVisible: true,
-            googleVisible: true,
-            spotifyVisible: true,
-            overcastVisible: true,
-            castboxVisible: true,
-            pocketcastsVisible: true,
-            castroVisible: true,
-            rssVisible: true,
-            radiopublicVisible: true,
-            breakerVisible: true
+            itunes : {},
+            links : {},
+            premium : false,
+            redirects: [],
+            sharableLink: "",
+            show: {}
         }
     })
 }
