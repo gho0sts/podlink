78d419ffd88f470738383e87544b60258cdb2e65
Nathan Gathright <nathan.gathright@gmail.com>
2019-01-09 15:26:59 -0600

    Revert "Update seo.js"
    This reverts commit 8ed886ea46125c9344c4c273ead6c0d70c9820e8.

--- servermiddleware/seo.js
+++ servermiddleware/seo.js
@@ -1,16 +1,26 @@
-const redirects = require('../premium.json')
+const redirects = require('../301.json')
 
 module.exports = function (req, res, next) {
   const url = req.url
-  let pathname = url
-
-  if(url.includes('?')){
-    pathname = url.substr(0, url.indexOf('?'))
-  }
-
+  const pathname = url.substr(0, url.indexOf('?'))
   const redirect = redirects.find(r => r.from === pathname)
   if (redirect) {
-    res.writeHead(301, { Location: redirect.to })
-    res.end()
+    if (redirect.autoforward && /iPad|iPhone|iPod|Android/.test(req.headers['user-agent'])) {
+      if (/iPad|iPhone|iPod/.test(req.headers['user-agent'])) {
+        console.log(`redirect: ${redirect.from} => ${redirect.to}`)
+        res.writeHead(302, { Location: redirect.autoforward.ios })
+        res.end()
+      } else {
+        console.log(`redirect: ${redirect.from} => ${redirect.to}`)
+        res.writeHead(302, { Location: redirect.autoforward.android })
+        res.end()
+      }
+    } else {
+      console.log(`redirect: ${redirect.from} => ${redirect.to}`)
+      res.writeHead(302, { Location: redirect.to })
+      res.end()
+    }
+  } else {
+    next()
   }
 }
