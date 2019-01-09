8ed886ea46125c9344c4c273ead6c0d70c9820e8
Nathan Gathright <nathan.gathright@gmail.com>
2019-01-09 15:25:14 -0600

    Update seo.js

--- servermiddleware/seo.js
+++ servermiddleware/seo.js
@@ -1,26 +1,16 @@
-const redirects = require('../301.json')
+const redirects = require('../premium.json')
 
 module.exports = function (req, res, next) {
   const url = req.url
-  const pathname = url.substr(0, url.indexOf('?'))
+  let pathname = url
+
+  if(url.includes('?')){
+    pathname = url.substr(0, url.indexOf('?'))
+  }
+
   const redirect = redirects.find(r => r.from === pathname)
   if (redirect) {
-    if (redirect.autoforward && /iPad|iPhone|iPod|Android/.test(req.headers['user-agent'])) {
-      if (/iPad|iPhone|iPod/.test(req.headers['user-agent'])) {
-        console.log(`redirect: ${redirect.from} => ${redirect.to}`)
-        res.writeHead(302, { Location: redirect.autoforward.ios })
-        res.end()
-      } else {
-        console.log(`redirect: ${redirect.from} => ${redirect.to}`)
-        res.writeHead(302, { Location: redirect.autoforward.android })
-        res.end()
-      }
-    } else {
-      console.log(`redirect: ${redirect.from} => ${redirect.to}`)
-      res.writeHead(302, { Location: redirect.to })
-      res.end()
-    }
-  } else {
-    next()
+    res.writeHead(301, { Location: redirect.to })
+    res.end()
   }
 }
